12c836e8ce8b3381482443ad13d41f4e96da636c
Lucas Jellema <Lucas.Jellema@amis.nl>
2018-03-27 18:02:22 +0200

    Adding Lab 8 - Spring Boot on Application Container Cloud

---
+++ lab 8/dronebuzzers/src/main/java/com/dronebuzzers/rest/part/Parts.java
@@ -0,0 +1,18 @@
+package com.dronebuzzers.rest.part;
+
+
+public class Parts {
+    
+    private Part[] parts;
+    
+    public Part[] getParts(){
+                
+        return parts;        
+    }
+    
+    public void setParts(Part[] parts){
+                
+        this.parts = parts;        
+    }
+    
+}
